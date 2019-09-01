1975f883b14a9d867d708438fca9a5cba847d847
Luiz Faitanin <lfaitanin@outlook.com>
2019-09-01 00:35:12 -0300

    Criando models, criando schema para user e sua mutation

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>NODE JS, EXPRESS JS, MONGODB, GRAPHQL, REACT JS<h1>
+<h2>Para rodar, basta rodar o comando docker-compose up -d para subir o container do mongo e da aplicação<h2>
+<h3>npm install<h3>
--- app.js
+++ app.js
@@ -2,29 +2,43 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql')
 const app = express();
-const { buildSchema } = require('graphql')
+const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
+
+const Event = require('./models/event');
+const User = require('./models/user');
 
 app.use(bodyParser.json());
-
-const events = [];
 
 app.use(
     '/graphql',
     graphqlHttp({
     schema: buildSchema(`
-        type Event{
+        type Event {
             _id: ID!
             title: String!
             description: String!
             price: Float!
             date: String!
         }    
+
+        type User {
+            _id: ID!
+            email: String!
+            password: String
+        }
         
-        input EventInput{
+        input EventInput {
             title: String!
             description: String!
             price: Float!
             date: String!
+        }
+
+        input UserInput {
+            email: String!
+            password: String!
         }
     
         type RootQuery {
@@ -33,6 +47,7 @@ app.use(
 
         type RootMutation {
             createEvent(eventInput: EventInput) : Event
+            createUser(userInput: UserInput) : User
         }
 
         schema {
@@ -41,22 +56,87 @@ app.use(
         }
     `),
     rootValue: {
-        events: (args) => {
-            return events;
-
+        events: () => {
+          return Event.find()
+            .then(events => {
+                return events.map(event => {
+                    return {...event._doc, _id: event.id };
+                })
+            })
+            
+            .catch(err =>{
+                console.log('error', err)
+            })
         },
-        createEvent: (args) => {
-           const event = {
-                _id: Math.random().toString(),
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: +args.eventInput.price,
-                date: args.eventInput.date
-           }
-           events.push(event);
-           return event;
+        createEvent: args => {
+           const event = new Event ({
+             title: args.eventInput.title,
+             description: args.eventInput.description,
+             price: +args.eventInput.price,
+             date: new Date(args.eventInput.date),
+             creator: '5d6b1d742fda1936fcd7a24f'
+            });
+            let createdEvent;
+            return event
+              .save()
+              .then(result => {
+                createdEvent = { ...result._doc, _id: result._doc._id.toString() };
+                return User.findById('5d6b1d742fda1936fcd7a24f');
+              })
+            .then(user => {
+                if(!user) {
+                    throw new Error('User not found.')
+                }
+                user.createdEvents.push(event)
+                return user.save();
+            })
+            .then(result =>{                
+                return createdEvent;
+           })
+           .catch(err => {
+               throw err;
+           });
+        },
+        createUser: args => {
+          return User.findOne({email:args.userInput.email}).then(user =>{
+             if(user) {
+                throw new Error('User exists already.')
+             }
+             return bcrypt
+             .hash(args.userInput.password, 12)
+           })
+            .then(hashedPassword =>{
+                const user = new User({
+                    email: args.userInput.email,
+                    password: hashedPassword
+                });
+                return user.save();
+            })
+            .then(result => {
+                return{...result._doc, password: null, _id: result.id };
+            })
+            .catch(err => {
+                throw err
+            })
         }
     }
 }));
 
-app.listen(3000);
+const options = {
+    autoIndex: false,
+    reconnectTries: 30, 
+    reconnectInterval: 500, 
+    poolSize: 10, 
+    bufferMaxEntries: 0
+  }
+  const connectWithRetry = () => {
+    console.log('MongoDB connection with retry')
+    mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-bauev.azure.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`, options).then(()=>{
+        console.log('MongoDB is connected')
+        app.listen(3000);
+    }).catch(err=>{
+      console.log('MongoDB connection unsuccessful, retry after 5 seconds.', err)
+      setTimeout(connectWithRetry, 5000)
+    })
+  }
+  connectWithRetry()
